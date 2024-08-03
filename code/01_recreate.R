a9d4594c7067782b507873207a09e3fe2c9daebd
vanichols <virginia.nichols@gmail.com>
2024-08-03 09:43:55 +0200

    add jesper data

---
+++ code/01_recreate.R
@@ -0,0 +1,19 @@
+# use the data jesper uses in the paper
+# make it so it will be easy to play with
+
+# inputs:
+# - replicates (start with 1)
+# - 9 intensities (make so can remove some)
+# - crop soil cover (use jesper starting values)
+# - weed density (use jesper values)
+# - Use Jesper assumptions for fitting Cousen's curve
+# - Crop yields at each soil cover (use his values)
+
+#--take all of these and calc optimum soil cover using
+#  a polynomial fit or something? unclear from his paper
+#  ANOVA to compare each of the intensities
+
+
+#--cousens curve note
+#--jesper uses a calculation that looks a little 
+# different compared to what he presents
